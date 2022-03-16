a9b1a1c86861de3c8ad9e40a5bb196a0b1913b92
Tommy Rocha <tomrocha99@gmail.com>
2022-03-15 23:07:43 -0700

    axios style pokemon api complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import './App.css';
 import Pokemon from './components/Pokemon';
+import axios from 'axios';
 
 
-
+// normal aka vanilla fetch method
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const ShowPokemon = () => {
@@ -13,6 +14,7 @@ function App() {
             return response.json()
         })
         .then(jsonResponse => {
+            console.log(jsonResponse);
             setPokemon({
                 allPokemon: jsonResponse.results
             })
@@ -20,9 +22,23 @@ function App() {
         .catch(err => console.log(err));
   };
 
+  //axios method
+  const axiousShowPokemon = () => {
+    console.log("Show Pokemon Axios Style!");
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=898')
+      .then((response) => {
+        console.log(response.data);
+        setPokemon({
+          allPokemon: response.data.results
+        })
+      })
+      .catch(err => console.log(err))
+  };
+
   return (
     <div className="App">
       <button onClick={ShowPokemon}>Show Me Pokemon</button>
+      <button onClick={axiousShowPokemon}>Show Me Pokemon Axios Style!</button>
       <Pokemon pokemon={pokemon}/>
     </div>
   );
